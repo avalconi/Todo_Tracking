78a9d9f1ed1f832d55ce91d17330f2735bcd22aa
avalconi <avalconi@proton.me>
2026-07-28 18:16:22 +0530

    init repo

---
+++ lib/domain/models/repository/todo_repo.dart
@@ -0,0 +1,15 @@
+/*
+functions of todo, get, add, update, delete
+*/ 
+
+import 'package:todo_flutter/domain/models/todo.dart';
+
+abstract class TodoRepo {
+  Future<List<Todo>> getTodos();
+
+  Future<void> addTodo(Todo newTodo);
+
+  Future<void> updateTodo(Todo todo);
+
+  Future<void> deleteTodo(Todo todo);
+}
